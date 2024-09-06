66b1333aef572a69020a7479d1b1880d32dea9c4
ramin <ramiinnss@gmail.com>
2024-09-06 19:33:39 +0330

    Remove cpu number banner

--- banner.go
+++ banner.go
@@ -11,7 +11,6 @@ func showBanner(listenAddr string) {
 	templ := fmt.Sprintf(`{{ .AnsiColor.BrightGreen }}{{ .Title "httpProxy" "" 8 }}
    {{ .AnsiColor.BrightCyan }}Author: {{ .AnsiColor.Yellow }}Ramin Sardari{{ .AnsiColor.Default }}
    {{ .AnsiColor.BrightCyan }}ListenAddr: {{ .AnsiColor.Yellow }}%s{{ .AnsiColor.Default }}
-   {{ .AnsiColor.BrightCyan }}NumCPU: {{ .AnsiColor.Yellow }}{{ .NumCPU }}{{ .AnsiColor.Default }}
    {{ .AnsiColor.BrightCyan }}Now: {{ .AnsiColor.Yellow }}{{ .Now "Mon, 02 Jan 2006 15:04:05 -0700" }}{{ .AnsiColor.Default }}`+"\n\n", listenAddr)
 
 	banner.InitString(colorable.NewColorableStdout(), true, true, templ)
